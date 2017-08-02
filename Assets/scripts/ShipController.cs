34e809061190323e88180cc1909fdfcb151f09ae
rex64 <rex64@users.noreply.github.com>
2017-08-02 22:06:31 +0200

    track

---
+++ Assets/scripts/ShipController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().MovePosition(transform.position + transform.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody>().MoveRotation(transform.rotation * Quaternion.Euler(0.0f, -1.0f, 0.0f));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody>().MoveRotation(transform.rotation * Quaternion.Euler(0.0f, 1.0f, 0.0f));
+        }
+    }
+}
