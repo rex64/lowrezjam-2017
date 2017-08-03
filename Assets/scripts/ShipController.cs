6138ae9643c952b26af2af2f12efa8589aaca533
rex64 <rex64@users.noreply.github.com>
2017-08-03 18:55:42 +0200

    no message

--- Assets/scripts/ShipController.cs
+++ Assets/scripts/ShipController.cs
@@ -14,7 +14,47 @@ public class ShipController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		if (Input.GetKey(KeyCode.Space))
+		//if (Input.GetKey(KeyCode.Space))
+  //      {
+  //          //GetComponent<Rigidbody>().MovePosition(transform.position + transform.forward * Time.deltaTime * speed);
+  //          GetComponent<Rigidbody>().AddForce(transform.forward * 10.0f);
+  //      }
+
+  //      if (Input.GetKey(KeyCode.A))
+  //      {
+  //          GetComponent<Rigidbody>().MoveRotation(transform.rotation * Quaternion.Euler(0.0f, -1.0f, 0.0f));
+  //      }
+  //      else if (Input.GetKey(KeyCode.D))
+  //      {
+  //          GetComponent<Rigidbody>().MoveRotation(transform.rotation * Quaternion.Euler(0.0f, 1.0f, 0.0f));
+  //      }
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 10))
+        {
+            print(hit.distance);
+            //transform.up = hit.normal;
+
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+
+            transform.position = hit.point;
+
+            transform.position = transform.position + (transform.up * 2.0f); 
+
+            //Vector3 lookAt = Vector3.Cross(-hit.normal, transform.right);
+            //// reverse it if it is down.
+            //lookAt = lookAt.y < 0 ? -lookAt : lookAt;
+            //// look at the hit's relative up, using the normal as the up vector
+            //transform.rotation = Quaternion.LookRotation(hit.point + lookAt, hit.normal);
+
+
+        }
+
+        if (Input.GetKey(KeyCode.Space))
         {
             //GetComponent<Rigidbody>().MovePosition(transform.position + transform.forward * Time.deltaTime * speed);
             GetComponent<Rigidbody>().AddForce(transform.forward * 10.0f);
@@ -23,22 +63,13 @@ public class ShipController : MonoBehaviour {
         if (Input.GetKey(KeyCode.A))
         {
             GetComponent<Rigidbody>().MoveRotation(transform.rotation * Quaternion.Euler(0.0f, -1.0f, 0.0f));
+            //GetComponent<Rigidbody>().add(-transform.right * 10.0f);
+
         }
         else if (Input.GetKey(KeyCode.D))
         {
             GetComponent<Rigidbody>().MoveRotation(transform.rotation * Quaternion.Euler(0.0f, 1.0f, 0.0f));
-        }
-    }
-
-    void FixedUpdate()
-    {
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, -transform.up, out hit, 1))
-        {
-            print(hit.distance);
-            transform.up = hit.normal;
-
+            //GetComponent<Rigidbody>().AddForce(transform.right * 10.0f);
 
         }
 
