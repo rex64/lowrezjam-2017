3bc119efb4aa6c932a1b41217cc514edc663bbe5
rex64 <rex64@users.noreply.github.com>
2017-08-05 12:07:55 +0200

    no message

---
+++ Assets/scripts/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform playerTransform;
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = playerTransform.position;
+        newPosition = newPosition + offset;
+        transform.position = newPosition;
+    }
+}
