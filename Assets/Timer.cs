0226e6bc56a7ce0e13b47c4d7904bdaecc546fbc
rex64 <rex64@users.noreply.github.com>
2017-08-06 12:24:05 +0200

    no message

---
+++ Assets/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        GetComponent<Text>().text = timer.ToString();
+	}
+}
